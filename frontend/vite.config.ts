4fa0cd44b08025c5e76e411d8a1c10de9d98f9f7
JuanRoman08 <juandiegoromansalazar08@gmail.com>
2025-07-08 21:55:52 +0000

    feat: login funcional, configuración de tailwind y contexto de usuario

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,21 +2,20 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// Detectar si estamos en desarrollo
 const isDev = process.env.NODE_ENV === 'development'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    proxy: isDev
-      ? {
+  server: isDev
+    ? {
+        proxy: {
           '/api': {
-            target: 'http://127.0.0.1:8000', // backend local
+            target: 'http://127.0.0.1:8000',
             changeOrigin: true,
           },
-        }
-      : undefined,
-  },
+        },
+      }
+    : undefined,
   build: {
     outDir: 'dist',
     sourcemap: false,
